1a77dfed51d593b7e2a0fb4a8fb2ac304590f4ac
rkilic1-spec <r.kilic1@gmail.com>
2026-01-12 15:20:37 +0300

    Add App component with basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Diyaliz Planlama Sistemi</h1>
+      <p>Web tabanlı operasyon ekranı</p>
+    </div>
+  )
+}
 
